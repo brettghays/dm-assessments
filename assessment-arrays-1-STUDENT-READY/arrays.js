92b3f1d44ca00c2da7c39a5a5e22183390006a89
brettghays <brett.g.hays.coding@gmail.com>
2018-04-21 13:28:26 -0600

    arrays1 done

--- assessment-arrays-1-STUDENT-READY/arrays.js
+++ assessment-arrays-1-STUDENT-READY/arrays.js
@@ -7,6 +7,7 @@
 //"red", "blue", "brown" in the array.
 
   //Code here
+  const rubberDucks = ["yellow","red", "blue", "brown"]
 
 
 //////////////////PROBLEM 2////////////////////
@@ -16,13 +17,13 @@
 //What is the first numbered index of an array?
 
   // var answer = 1
-  // var answer = 0
+   var answer = 0
 
 //How do you access the length of an array, such as the array
 //you created above called rubberDucks?
 
   // var arrayLength = Array.length(rubberDucks)
-  // var arrayLength = rubberDucks.length
+   var arrayLength = rubberDucks.length
   // var arrayLength = rubberDucks.len
   // var arrayLength = rubberDucks.length()
 
@@ -35,6 +36,7 @@
 //The passed-in array should NOT be modified.
 
   //Code here
+  let getLast = arr => arr[arr.length-1]
 
 
 //////////////////PROBLEM 4////////////////////
@@ -46,6 +48,8 @@
 
   //Code here
 
+  let getElement = (arr, index) => arr[index]
+
 
 //////////////////PROBLEM 5////////////////////
 
@@ -55,6 +59,7 @@
 //to the given value. You do not need to return the array.
 
   //Code here
+  let changeElement = (arr,index,value) => arr.splice(index,1,value);
 
 
 //////////////////PROBLEM 6////////////////////
@@ -63,3 +68,7 @@
 //the last element. trim should return the array.
 
   //Code here
+  let trim = arr => {
+    arr.pop()
+    return arr
+  }
