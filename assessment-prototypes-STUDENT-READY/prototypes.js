a59dcb822278c7b3f7c775289510a020d064c89a
Brett Hays <brett.g.hays.coding@gmail.com>
2018-04-21 20:48:24 -0600

    prototypes finished

--- assessment-prototypes-STUDENT-READY/prototypes.js
+++ assessment-prototypes-STUDENT-READY/prototypes.js
@@ -4,6 +4,9 @@
 //Create a prototype for a String called reverse that returns the string but, like, reversed.
 
 //Code here
+String.prototype.reverse = function() {
+  return this.split('').reverse().join('');
+}
 
 
 //////////////////PROBLEM 2////////////////////
@@ -22,4 +25,17 @@ function Person(name, age) {
 }
 
 //Code here
+Person.prototype.drinkCoffee = function() {
+  this.energy += 10;
+  this.linesOfCode += 50;
+  this.qualityOfCode -= 1;
+  return this;
+}
+
+Person.prototype.readDocumentation = function() {
+  this.energy -= 20;
+  this.linesOfCode -= 30;
+  this.qualityOfCode += 3;
+  return this;
+}
 
